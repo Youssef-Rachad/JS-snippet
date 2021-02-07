62c866b6f292b58ade7ab88b566914c8840667e7
Youssef-Rachad <rachadyoussef26@gmail.com>
2021-02-06 19:27:58 -0500

    Update correctBracing.js

--- correctBracing.js
+++ correctBracing.js
@@ -1,27 +1,31 @@
 /*
-Daily Coding Problem: Problem #27
+--Daily Coding Problem: Problem #27--
 Given a string of round, curly, and square open and closing brackets, return whether the brackets are balanced (well-formed).
 
 For example, given the string "([])[]({})", you should return true.
 
 Given the string "([)]" or "((()", you should return false.
+
 done on 06/02/2021
+note: the error messages interfere with returning false,
+      however they are helpful to know where the string fails 
 */
 function solution(word){
-let open=[];  
-for(letter of word){
-    if (letter.match(/(\{|\(|\[)/) != null){
-      open.push(letter);
-    }else if (letter.match(/(\}|\)|\])/) != null && open[open.length-1] == {"}":"{",")":"(","]":"["}[letter]){
-      open.pop();
-    }else{      
-//console.log(`Error: failed at character '${letter}'\nMatched '${{"}":"{",")":"(","]":"["}[letter]}' against '${open[open.length-1]}'\nCurrent stack: ${open}`);
-return false;
+    let open=[];  
+    for(letter of word){
+        if (letter.match(/(\{|\(|\[)/) != null){
+            open.push(letter);
+        }else if (letter.match(/(\}|\)|\])/) != null && open[open.length-1] == {"}":"{",")":"(","]":"["}[letter]){
+            open.pop();
+        }else{      
+            //console.log(`Error: failed at character '${letter}'\nMatched '${{"}":"{",")":"(","]":"["}[letter]}' against '${open[open.length-1]}'\nCurrent stack: ${open}`);
+            return false;
+        }
     }
-  }
-if (open.length != 0){
-  //console.log(`Error: incomplete braces\nCurrent stack: ${open}`);
-return false;
-}else{
-  return true;
-}}
+    if (open.length != 0){
+        //console.log(`Error: incomplete braces\nCurrent stack: ${open}`);
+        return false;
+    }else{
+        return true;
+    }
+}
